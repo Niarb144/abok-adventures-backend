3863f755ffc8a18f5780d19b7248a4a51498dbc2
Teddy <teddybrian543@gmail.com>
2026-03-02 11:00:58 +0300

    Fix Cloudinary Key Visibility

--- src/config/cloudinary.js
+++ src/config/cloudinary.js
@@ -1,11 +1,18 @@
 import { v2 as cloudinary } from "cloudinary";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+const cloud_name = process.env.CLOUDINARY_CLOUD_NAME;
+const api_key = process.env.CLOUDINARY_API_KEY;
+const api_secret = process.env.CLOUDINARY_API_SECRET;
 
 cloudinary.config({
-  cloud_name: "dbw17hrqd",
-  api_key: "578986455996639",
-  api_secret: "wWKRCSD_0FTxbadXCPEIwvtUE8s",
+  cloud_name: cloud_name,
+  api_key: api_key,
+  api_secret: api_secret,
 });
 
-console.log("✅ Cloudinary configured" + ` (cloud: ${process.env.CLOUDINARY_CLOUD_NAME})` + ` (key: ${process.env.CLOUDINARY_API_KEY})` + ` (secret: ${process.env.CLOUDINARY_API_SECRET ? "***" : "NOT SET"})`);
+console.log("✅ Cloudinary configured");
 
 export default cloudinary;
