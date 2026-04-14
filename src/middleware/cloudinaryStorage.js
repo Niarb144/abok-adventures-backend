76f74629b4aacb553758fead10972cbbaf3ce3d4
Teddy <teddybrian543@gmail.com>
2026-04-14 14:27:44 +0300

    Remove typescript checking

--- src/middleware/cloudinaryStorage.js
+++ src/middleware/cloudinaryStorage.js
@@ -1,8 +1,7 @@
+// @ts-nocheck
 import multer from "multer";
-// import { CloudinaryStorage } from "multer-storage-cloudinary";
+import { CloudinaryStorage } from "multer-storage-cloudinary";
 import cloudinary from "../config/cloudinary.js";
-
-const { CloudinaryStorage } = require('multer-storage-cloudinary');
 
 const storage = new CloudinaryStorage({
   cloudinary,
